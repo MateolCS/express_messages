ec80bd32ec284d40633f1d73ba4eb0af05c1b0a3
MateoLCS <mateusz.namysl0318@gmail.com>
2022-11-06 22:46:30 +0100

    Finished project, added /new route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "test",
+    user: "Maria",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", function (req, res, next) {
+  const data = req.body;
+  messages.push({ text: data.text, user: data.user, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
